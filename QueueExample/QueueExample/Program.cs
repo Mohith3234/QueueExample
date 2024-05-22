20afae8ae510c8b6376c94ec6cfb7be275edbbb8
Koninti Mohithkumar Goud <al6127@accionlabs.in>
2024-05-22 17:32:30 +0530

    first commit

---
+++ QueueExample/QueueExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+namespace QueueExample
+{
+   class Program
+    {
+        static void Main()
+        {
+            Queue<string> queue= new Queue<string>();
+            //add element to queue
+            queue.Enqueue("Task1");
+            queue.Enqueue("Task2");
+            queue.Enqueue("Task3");
+            queue.Enqueue("Task4");
+            queue.Enqueue("Task5");
+
+            //remove element from queue
+            string s=queue.Dequeue();
+            Console.WriteLine("Remove element From Queue : "+s);
+            
+            //Retrive the Element from queue
+            String s1=queue.Peek();
+            Console.WriteLine("retrive front element from queue : " + s1);
+            foreach(var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+           
+            Console.ReadKey();
+        }
+    }
+}
